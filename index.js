1a78e9cc516f877899ad860df325106838e1af81
RiteshDafale <96180597+RiteshDafale@users.noreply.github.com>
2023-08-01 19:40:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+console.log('Project is made by Ritesh Dafale');
+
+let yellow = document.getElementById("yellow");
+yellow.addEventListener("click",function(){
+   document.body.style.background = "yellow";
+})
+
+let green = document.getElementById("green");
+green.addEventListener("click",function(){
+   document.body.style.background = "green";
+})
+
+let red = document.getElementById("red");
+red.addEventListener("click",function(){
+   document.body.style.background = "red";
+})
+
+let white = document.getElementById("white");
+white.addEventListener("click",function(){
+   document.body.style.background = "white";
+})
